2e411dccfcde58ff889e2f02a0a94b0353a7419b
M <73000448+maaarrk122@users.noreply.github.com>
2020-10-22 23:02:25 -0400

    Add files via upload

---
+++ d2bs/kolbot/libs/horde/builds/Barbarian/shoutingtank.js
@@ -0,0 +1,16 @@
+/**
+*	@filename	shoutingtank.js
+*	@author		Adpist
+*	@desc		Battle orders / shout / 1 pt war cry / defense passives barb USES tiered pickits
+*	@credits	Adpist, Mark, JeanMax / SiC-666 / Dark-f, Alogwe, Imba, Kolton, Larryw, Noah, QQValpen, Sam, YGM
+*
+*/
+
+var HordeBuild = {
+	statsBuild: "Levelling",
+	skillsBuild: "ShoutingTank",
+	autoBuildTemplate: "ShoutingTank",
+	mercAct2Normal: "Prayer",
+	mercAct2Nightmare: "Holy Freeze",
+	pickits: ["horde/tier/common.tiered.xpac.nip", "horde/tier/barbarian.tiered.shouting.xpac.nip", "horde/merc.act2.meditation.xpac.nip"]
+};
