2753750853492dedc90282673daad3485d2cedaa
M <73000448+maaarrk122@users.noreply.github.com>
2020-10-22 23:59:14 -0400

    Add files via upload

---
+++ d2bs/kolbot/libs/horde/builds/Necromancer/corpseresisttiered.js
@@ -0,0 +1,16 @@
+/**
+*	@filename	corpseresisttiered.js
+*	@author		Adpist
+*	@desc		Summoner / Lower resist necromancer build
+*	@credits	Adpist, Mark, JeanMax / SiC-666 / Dark-f, Alogwe, Imba, Kolton, Larryw, Noah, QQValpen, Sam, YGM
+*
+*/
+
+var HordeBuild = {
+	statsBuild: "Levelling",
+	skillsBuild: "CorpseResist",
+	autoBuildTemplate: "CorpseResist",
+	mercAct2Normal: "Prayer",
+	mercAct2Nightmare: "Might",
+	pickits: ["horde/tier/common.tiered.xpac.nip", "horde/tier/necro.tiered.corpse.xpac.nip", "horde/merc.act2.meditation.xpac.nip"]
+};
