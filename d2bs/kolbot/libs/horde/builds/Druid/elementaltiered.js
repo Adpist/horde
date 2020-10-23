cdaca0d5c8ae2d324f2cebb5032cacfcd2c35e4f
M <73000448+maaarrk122@users.noreply.github.com>
2020-10-23 00:01:08 -0400

    Add files via upload

---
+++ d2bs/kolbot/libs/horde/builds/Druid/elementaltiered.js
@@ -0,0 +1,16 @@
+/**
+*	@filename	elementaltiered.js
+*	@author		Adpist
+*	@desc		Wind(Tornado & Hurricane) & Oak Sage Druid
+*	@credits	Adpist, Mark, JeanMax / SiC-666 / Dark-f, Alogwe, Imba, Kolton, Larryw, Noah, QQValpen, Sam, YGM
+*
+*/
+
+var HordeBuild = {
+	statsBuild: "Levelling",
+	skillsBuild: "Elemental",
+	autoBuildTemplate: "Elemental",
+	mercAct2Normal: "Prayer",
+	mercAct2Nightmare: "Holy Freeze",
+	pickits: ["horde/tier/common.tiered.xpac.nip", "horde/tier/druid.tiered.element.xpac.nip", "horde/merc.act2.meditation.xpac.nip"]
+};
