f651266e9352f91013ec31d7cb60331e7517d72d
M <73000448+maaarrk122@users.noreply.github.com>
2020-10-22 23:58:21 -0400

    Add files via upload

---
+++ d2bs/kolbot/libs/horde/builds/Paladin/hammerconvictiontiered.js
@@ -0,0 +1,15 @@
+/**
+*	@filename	hammerconviction.js
+*	@author		Adpist
+*	@desc		Hammer / Conviction paladin build
+*	@credits	Adpist, JeanMax / SiC-666 / Dark-f, Alogwe, Imba, Kolton, Larryw, Noah, QQValpen, Sam, YGM
+*
+*/
+
+var HordeBuild = {
+	statsBuild: "LevellingMana",
+	skillsBuild: "HammerConviction",
+	autoBuildTemplate: "HammerConviction",
+	mercAct2Normal: "Defiance",
+	pickits: ["horde/tier/common.tiered.xpac.nip", "horde/tier/paladin.tiered.hammerconviction.xpac.nip", "horde/merc.act2.meditation.xpac.nip"]
+};
