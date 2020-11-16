782fcaff03791f5f0da3e8f160d23c403b3f6fd8
M <73000448+maaarrk122@users.noreply.github.com>
2020-11-16 02:25:11 -0500

    Add files via upload

---
+++ d2bs/kolbot/libs/horde/builds/Amazon/Javazon.js
@@ -0,0 +1,16 @@
+/**
+*	@filename	elementaltiered.js
+*	@author		Adpist
+*	@desc		Wind(Tornado & Hurricane) & Oak Sage Druid
+*	@credits	Adpist, Mark, JeanMax / SiC-666 / Dark-f, Alogwe, Imba, Kolton, Larryw, Noah, QQValpen, Sam, YGM
+*
+*/
+
+var HordeBuild = {
+	statsBuild: "Levelling",
+	skillsBuild: "Java",
+	autoBuildTemplate: "Javazon",
+	mercAct2Normal: "Prayer",
+	mercAct2Nightmare: "Holy Freeze",
+	pickits: ["horde/tier/common.tiered.xpac.nip", "horde/follower.nip", "horde/tier/amazon.tiered.java.xpac.nip", "horde/merc.act2.meditation.xpac.nip"]
+};
