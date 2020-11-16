7621f257906c8362220f417877bf953f70186577
M <73000448+maaarrk122@users.noreply.github.com>
2020-11-16 01:51:32 -0500

    Add files via upload

--- d2bs/kolbot/libs/horde/builds/templates/skills/Assassin.js
+++ d2bs/kolbot/libs/horde/builds/templates/skills/Assassin.js
@@ -1,25 +1,29 @@
 /**
-*	@filename	Assassin.js
-*	@author		cody
+*	@filename	Druid.js
+*	@author		Adpist
 *	@desc		Druid skill builds :
 *					"Wind" : Wind druid with max oak
 *	@credits	Adpist, Mark, JeanMax / SiC-666 / Dark-f, Alogwe, Imba, Kolton, Larryw, Noah, QQValpen, Sam, YGM
-*
+*	
 */
 
 var SkillsBuilds = {
 	"Trapsin": [
-		[251, 20, false], //Fireblast
+		[251, 5, false], //Fireblast +5
 		[252, 1, false], //Claw mastery
-		[258, 1, false], //Burst of speed
+		[258, 1, false], //Burst of speed +1
 		[253, 1, false], //Physical hammer
-		[262, 20, false], //Wake of fire
+		[251, 3, false], //Fireblast +3
+		[262, 5, false], //Wake of fire +5
+		[258, 4, false], //Burst of speed +4
+		[262, 15, false], //Wake of fire +15
+		[251, 12, false], //Fireblast +12
 		[264, 1, false], //Cloak of shadows
 		[263, 1, false], //Weapon block
 		[268, 1, false], //Shadow master
-		[267, 1, false], //Fade
 		[272, 20, false], //Inferno sentry
 		[273, 1, false], //Mind blast
 		[279, 1, false], //Shadow master
+		[258, 15, false], //Burst of speed +15
 	]
 };
